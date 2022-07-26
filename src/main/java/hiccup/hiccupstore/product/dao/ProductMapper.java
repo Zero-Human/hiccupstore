757b76b612263445e145b7c7acf7d4fe94e4513b
yummygyudon <bang2brew@gmail.com>
2022-07-26 11:30:05 +0900

    save(2)

--- src/main/java/hiccup/hiccupstore/product/dao/ProductMapper.java
+++ src/main/java/hiccup/hiccupstore/product/dao/ProductMapper.java
@@ -9,13 +9,31 @@ import java.util.ArrayList;
 @Mapper
 @Repository
 public interface ProductMapper {
-    void insert(Product p) ;
-    Product selectById(int p_id) ;
-    ArrayList<Product> selectByCategory(int c_id) ;
-    ArrayList<Product> selectByName(String p_name) ;
-    ArrayList<Product> selectByName(String p_name) ;
-    ArrayList<Product> selectByName(String p_name) ;
-    ArrayList<Product> selectByName(String p_name) ;
-    ArrayList<Product> selectByName(String p_name) ;
+
+    void insert(Product product) ;
+
+
+    // 상세 상품 조회
+    Product selectById(int productId) ;
+
+
+    // 상품 조회
+    ArrayList<Product> selectByCategory(int categoryId) ;
+
+    /* Parameter : int 타입 _ x : x만원대
+     * Return : ArrayList<Product>
+     */
+    ArrayList<Product> selectByPriceRange(int priceRange) ;
+
+    /* Parameter : int 타입 _ 카테고리 ID
+     * Return : ArrayList<Product>
+     * 설명
+     * 상단 간편검색창으로 검색했을 경우
+     * 세트 상품/유사이름 상품 (ex."안동소주" 검색 -> "명인 안동소주" , "안동소주 일품" )
+     * -> Product 반환이 아닌 ArrayList로 반환
+     */
+    ArrayList<Product> selectByName(String productName) ;
+
+
 
 }
