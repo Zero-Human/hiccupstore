d34f5fe76e45bdd9821ae364e9d24fcbfa430089
박주현 <leehot2665@naver.com>
2022-09-06 11:30:53 +0900

    delvelop FIX: 테스트 중에 문제 발생한 것들 수정

--- src/main/java/hiccup/hiccupstore/board/service/BoardService.java
+++ src/main/java/hiccup/hiccupstore/board/service/BoardService.java
@@ -38,6 +38,9 @@ public class BoardService {
             boardMapper.deleteImageByBoardId(productQnA.getBoardId());
             boardMapper.insertImage(imageList);
         }
+        else{
+            boardMapper.deleteImageByBoardId(productQnA.getBoardId());
+        }
         boardMapper.editProductQnA(productQnA);
 
     }
